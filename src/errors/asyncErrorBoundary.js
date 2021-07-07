f1f1c4646592060c32e072c34078f3aab55778f8
ddoshi2021@gmail.com <ddoshi2021@gmail.com>
2021-07-07 09:00:55 -0700

    Error handling for asyncErrorBoundary

--- src/errors/asyncErrorBoundary.js
+++ src/errors/asyncErrorBoundary.js
@@ -1,11 +1,15 @@
-import React from 'react'
-
-function asyncErrorBoundary() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default asyncErrorBoundary
+function asyncErrorBoundary(delegate, defaultStatus) {
+    return (request, response, next) => {
+      Promise.resolve()
+        .then(() => delegate(request, response, next))
+        .catch((error = {}) => {
+          const { status = defaultStatus, message = error } = error;
+          next({
+            status,
+            message,
+          });
+        });
+    };
+  }
+  
+  module.exports = asyncErrorBoundary;
