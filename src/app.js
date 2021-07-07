9368f0220b9d1bd83e4bf01826fecaf08b9828d2
ddoshi2021@gmail.com <ddoshi2021@gmail.com>
2021-07-07 09:11:15 -0700

    update with router and error handler

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+// Require Router 
+const moviesRouter = require("./movies/movies.router");
+const theatersRouters = require("./theaters/theaters.router")
+const reviewsRouter = require("./reviews/reviews.router")
+
+//require error
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler")
+
 
 module.exports = app;
